295c4d5cce109c395bb4a195de698a675b6ee61e
Chris Reilly <christopher.reilly@reddit.com>
2026-01-13 15:29:58 -0700

    fix merge issues

--- sidebars.ts
+++ sidebars.ts
@@ -283,11 +283,6 @@ const sidebars: SidebarsConfig = {
       ],
     },
     {
-      type: "doc",
-      label: "Text Fallback",
-      id: "capabilities/server/text_fallback",
-    },
-    {
       type: "category",
       label: "Migration Guides",
       items: [
